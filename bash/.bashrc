75e22064af0bc7ab3886cb63beadf32a426ed14e
ubyt3m3 <ubyt3m3@gmail.com>
2018-04-23 02:04:59 -0400

    Added ranger dir, updated bash/.Xresources and bash/.bashrc

--- bash/.bashrc
+++ bash/.bashrc
@@ -2,11 +2,17 @@ ID=`id -u`
 if [[ "${ID}" -eq 0 ]]; then
    PS1='\]\e[01;35m\]root\]\e[00;31m\]@\h\[\e[00;37m\][\[\e[01;34m\]\w\[\e[00;37m\]]\n\[\e[47m\]\[\e[1;30m\]#\[\033[00m\] '
 else
-   PS1='\[\e[00;32m\]\A \[\e[00;91m\]\u\[\e[01;93m\]@\h\[\e[00;37m\][\[\e[01;34m\]\w\[\e[00;37m\]]\n\[\e[47m\]\[\e[1;30m\]$\[\e[00m\] '
+   PS1='\[\e[38;5;106m\]\A \[\e[38;5;210m\]\u\[\e[38;5;229m\]@\h\[\e[38;5;252m\][\[\e[38;5;69m\]\w\[\e[38;5;252m\]]\n\[\e[47m\]\[\e[38;5;8m\]$\[\e[00m\] '
+   #PS1='\[\e[00;32m\]\A \[\e[00;91m\]\u\[\e[01;93m\]@\h\[\e[00;37m\][\[\e[01;34m\]\w\[\e[00;37m\]]\n\[\e[47m\]\[\e[1;30m\]$\[\e[00m\] '
 fi
 
+# Slackware default for $LANG is en_US
+# To support displaying Japanese while still getting 
+# messages in English, set below two env variables
+export LANG=ja_JP.UTF-8
+export LC_MESSAGES=en_US
 export EDITOR=nvim
-export MPD_HOST=/home/ubyt3m3/.config/mpd/socket
+#export MPD_HOST=/home/ubyt3m3/.config/mpd/socket
 
 PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 
---
+++ i3/i3pystatusconfig.py
@@ -0,0 +1,148 @@
+from i3pystatus import Status
+
+status = Status(logfile=$HOME/var/i3pystatus.log)
+
+# Displays clock like this:
+# Tue 30 Jul 11:59:46 PM KW31
+#                          ^-- calendar week
+status.register("clock",
+    format="%b %e (%a)  %H:%M %p ",
+    color="#fffff3",
+    interval=1,
+    )
+
+# Shows the average load of the last minute and the last 5 minutes
+# (the default value for format is used)
+#status.register("load")
+
+# Show CPU usage
+status.register("cpu_usage",
+    format="  {usage:03.0f}%",
+    color="#fffff3",
+    interval=1,
+    )
+# Shows your CPU temperature, if you have a Intel CPU
+#status.register("temp",
+#    format=" {temp:.0f}°C",
+#    color="#fffff3",
+#    )
+
+# The battery monitor has many formatting options, see README for details
+
+# This would look like this, when discharging (or charging)
+# ↓14.22W 56.15% [77.81%] 2h:41m
+# And like this if full:
+# =14.22W 100.0% [91.21%]
+#
+# This would also display a desktop notification (via D-Bus) if the percentage
+# goes below 5 percent while discharging. The block will also color RED.
+# If you don't have a desktop notification demon yet, take a look at dunst:
+#   http://www.knopwob.org/dunst/
+#status.register("battery",
+#    battery_ident="BAT0",
+#    format="{status} {percentage:.0f}%",
+#    color="#fffff3",
+#    alert=True,
+#    alert_percentage=5,
+#    status={
+#        "DIS": "↓",
+#        "CHR": "↑",
+#        "FULL": "=",
+#    },
+#)
+#
+## This would look like this:
+## Discharging 6h:51m
+#status.register("battery",
+#    format="{status} {remaining:%E%hh:%Mm}",
+#    alert=True,
+#    alert_percentage=5,
+#    status={
+#        "DIS":  "Discharging",
+#        "CHR":  "Charging",
+#        "FULL": "Bat full",
+#    },)
+
+# Displays whether a DHCP client is running
+#status.register("runwatch",
+#    name="DHCP",
+#    path="/var/run/dhclient*.pid",)
+
+# Shows the address and up/down state of eth0. If it is up the address is shown in
+# green (the default value of color_up) and the CIDR-address is shown
+# (i.e. 10.10.10.42/24).
+# If it's down just the interface name (eth0) will be displayed in red
+# (defaults of format_down and color_down)
+#
+# Note: the network module requires PyPI package netifaces
+status.register("network",
+    interface="eth0",
+    color_up="#fffff3",
+    color_down="#6e6e6e",
+    format_up="  {v4cidr}",
+    format_down="  Down",)
+
+# Note: requires both netifaces and basiciw (for essid and quality)
+status.register("network",
+    interface="wlo1",
+    color_up="#fffff3",
+    color_down="#6e6e6e",
+    format_up="  {essid} ({quality:03.0f}%) {v4cidr}",
+    format_down="  Down",)
+
+# Shows disk usage of /home
+# Format:
+# 42/128G
+status.register("disk",
+    path="/home",
+    format="  {used} / {total}GB",)
+
+# Shows disk usage of /
+# Format:
+# 42/128G
+status.register("disk",
+    path="/",
+    format="  {used} / {total}GB",
+    color="#fffff3",
+#    hints={"separator": False, "seprator_block_width": 5},
+    )
+
+# Shows memory usage
+status.register("mem",
+    format="  {used_mem} / {total_mem}GB",
+    color="#fffff3",
+    warn_color="#e5e500",
+    alert_color="#ff1919",
+    divisor=1073741824,
+    )
+
+# Shows pulseaudio default sink volume
+#
+# Note: requires libpulseaudio from PyPI
+status.register("pulseaudio",
+    format="  {volume}%",
+    color_muted="#6e6e6e",
+    color_unmuted="#fffff3",
+    format_muted="  Muted",)
+
+# Shows mpd status
+# Format:
+# Cloud connected▶Reroute to Remain
+#status.register("mpd",
+#    format="{title}{status}{album}",
+#    status={
+#        "pause": "▷",
+#        "play": "▶",
+#        "stop": "◾",
+#    },)
+
+# Show cmus status
+status.register("cmus",
+    format="  {title}  / {artist}",
+    format_not_running="  Not running", 
+    color="#fffff3",
+    color_not_running="#6e6e6e",
+    )
+
+
+status.run()
